e19dde5702801360ec8ad4ba0fc2b5561f6ee4b0
dhust <rafaelmoura.dev@gmail.com>
2016-07-25 15:42:17 -0300

    added a little debug helper code
    > max

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <inttypes.h>
 #include <Utix/ScopeExit.h>
 #include "Machine.h"
 
 
-
+int Debug(gbx::Machine* const machine);
 
 int main(int argc, char** argv) {
 	if(argc < 2) {
@@ -24,11 +25,71 @@ int main(int argc, char** argv) {
 	if(!machine->LoadRom(argv[1]))
 		return EXIT_FAILURE;
 
-	while (machine->Step()) {
+
+
+	return Debug(machine);
+}
+
+
+
+
+
+
+
+
+
+int Debug(gbx::Machine* const machine) {
+	printf("Select an option:\n"              \
+	       "q - quit\n"                       \
+	       "s - 1 step\n"                     \
+	       "c - step until sig or pc break\n" \
+	       "b - set pc break\n");
+
+	char opt = 0;
+	unsigned int pc_wanted = 0;
+	
+	while(true) {
+		scanf(" %c", &opt);
+		
+		switch(opt)
+		{
+		
+		case 's':
+			if(!machine->Step()) 
+				return EXIT_FAILURE;
+			break;
+			
+		case 'c':
+			while(machine->cpu.GetPC() != pc_wanted) {
+				if(!machine->Step()) 
+					return EXIT_FAILURE;
+			}
+			break;
+		
+		case 'b':
+			printf("selected desired pc: ");
+			scanf(" %x", &pc_wanted);
+			printf("\nselected: %x\n", pc_wanted);
+			if(pc_wanted > gbx::HOME_MAX_OFFSET)
+				fprintf(stderr, "warning pc_wanted oveflows HOME memory offsets\n");
+			break;
+		
+		case 'q':
+			goto break_while;
+		
+		default:
+			fprintf(stderr, "error, not opt \'%c\'\n", opt);
+			break;
+		}
 
 	}
 
+break_while:
+
 	return EXIT_SUCCESS;
 }
+
+
+
 
 
