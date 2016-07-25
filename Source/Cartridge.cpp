f53203e3d7c2c604f966bdc2bf7b395f39701776
dhust <rafaelmoura.dev@gmail.com>
2016-07-25 03:19:08 -0300

    added revision
    revision
    
    vs update
    
    size fix

--- Source/Cartridge.cpp
+++ Source/Cartridge.cpp
@@ -13,6 +13,13 @@ namespace gbx {
 
 
 
+inline uint8_t* AllocateCartridgeData(size_t bytes);
+inline void DeallocateCartridgeData(uint8_t* ptr);
+inline size_t GetCartridgeDataSize(uint8_t* ptr);
+
+
+
+
 
 
 
@@ -37,27 +44,17 @@ bool Cartridge::Load(const char* file_name) {
 		return false;
 	}
 
-	
-	this->Dispose();
 
-	// alloc enough bytes for rom_file and for a size_t variable
-	const size_t m_data_size = (sizeof(uint8_t) * rom_size) + sizeof(size_t);
-	const_cast<uint8_t*&>(m_data) = static_cast<uint8_t*>(malloc(m_data_size));
-
-	if (m_data == nullptr) {
-		perror("Couldn't allocate memory for ROM");
-		return false;
-	}
-
-	auto cartridge_data_guard = utix::MakeScopeExitIf([=] {
+	if(GetSize() < rom_size) {
 		this->Dispose();
-	});
-
-	// store Cartridge's size into first bytes of m_data, then
-	// increment m_data pointer to not erase it
-	*reinterpret_cast<size_t*>(m_data) = rom_size;
-	const_cast<uint8_t*&>(m_data) = reinterpret_cast<uint8_t*>(reinterpret_cast<size_t*>(m_data) + 1);
+		const_cast<uint8_t*&>(m_data) = AllocateCartridgeData(sizeof(uint8_t) * rom_size);
 		
+		if (m_data == nullptr) {
+			perror("Couldn't allocate memory for ROM");
+			return false;
+		}
+	}
+	
 	fread(m_data, sizeof(uint8_t), rom_size, rom_file);
 
 	if (ferror(rom_file)) {
@@ -69,8 +66,7 @@ bool Cartridge::Load(const char* file_name) {
 	// just in case.
 	if (m_data[0x142] != 0)
 		m_data[0x142] = 0;
-
-	cartridge_data_guard.Cancel();
+	
 	return true;
 }
 
@@ -79,9 +75,7 @@ bool Cartridge::Load(const char* file_name) {
 
 void Cartridge::Dispose() {
 	if(m_data) {
-		// gives free the original pointer address
-		// m_data - sizeof size_t  bytes
-		free(reinterpret_cast<size_t*>(m_data) - 1);
+		DeallocateCartridgeData(m_data);
 		const_cast<uint8_t*&>(m_data) = nullptr;
 	}
 }
@@ -106,7 +100,6 @@ GameBoyType Cartridge::GetGameBoyType() const {
 
 
 
-
 const char* Cartridge::GetName() const {
 	return reinterpret_cast<const char*>(m_data + 0x134);
 }
@@ -114,9 +107,57 @@ const char* Cartridge::GetName() const {
 
 
 
+
 size_t Cartridge::GetSize() const {
-	return *(reinterpret_cast<size_t*>(m_data) - 1);
+	return GetCartridgeDataSize(m_data);
 }
+
+
+
+
+
+
+
+
+
+// putting rein..casts into one place
+
+
+inline uint8_t* AllocateCartridgeData(size_t bytes) {
+	// alloc enough bytes for rom_file and for a size_t variable
+	const auto size = (sizeof(uint8_t) * bytes);
+	size_t* const ptr = static_cast<size_t*>(malloc(size + sizeof(size_t)));
+	
+	if(ptr) {
+		// store Cartridge's size into first bytes of the array, then
+		// increment pointer to not erase it
+		*ptr = size;
+		return reinterpret_cast<uint8_t*>(ptr + 1);
+	}
+	
+	return reinterpret_cast<uint8_t*>(ptr);
+}
+
+
+inline void DeallocateCartridgeData(uint8_t* ptr) {
+	// free the original pointer address
+	free(reinterpret_cast<size_t*>(ptr) - 1);
+}
+
+
+
+
+inline size_t GetCartridgeDataSize(uint8_t* ptr) {
+	// retrieve size from array first bytes as size_t
+	return ptr ? *(reinterpret_cast<size_t*>(ptr) - 1) : 0;
+}
+
+
+
+
+
+
+
 
 
 
