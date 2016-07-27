56f763128c85c062c93cc3a25a2ffe412266b616
Rafael Moura <rafaelmoura.dev@gmail.com>
2016-07-26 22:20:15 -0300

    inline/constexpr common revision

--- Source/Common.h
+++ Source/Common.h
@@ -10,25 +10,40 @@ constexpr size_t operator""_Mib(unsigned long long mb) { return static_cast<size
 
 
 
-
-inline uint16_t ConcatBytes(const uint8_t high_byte, const uint8_t low_byte) {
+constexpr uint16_t ConcatBytes(const uint8_t high_byte, const uint8_t low_byte) {
 	return (high_byte << 8) | low_byte;
 }
 
 
+constexpr uint8_t GetLowByte(const uint16_t value) {
+	return value & 0x00FF;
+}
+
+
+constexpr uint8_t GetHighByte(const uint16_t value) {
+	return (value & 0xFF00) >> 8;
+}
+
+
+
+constexpr uint8_t GetLowNibble(uint8_t byte) {
+	return byte & 0x0F;
+}
+
+
+
+constexpr uint8_t GetHighNibble(uint8_t byte) {
+	return byte & 0xF0;
+}
 
 
 
 
 
 inline void Split16(const uint16_t value, uint8_t* const high_byte, uint8_t* const low_byte) {
-	*high_byte = (value & 0xFF00) >> 8;
-	*low_byte = (value & 0x00FF);
+	*high_byte = GetHighByte(value);
+	*low_byte = GetLowByte(value);
 }
-
-
-
-
 
 
 
@@ -36,10 +51,6 @@ inline void Add16(const uint16_t val, uint8_t* const high_byte, uint8_t* const l
 	const uint16_t result = ConcatBytes(*high_byte, *low_byte) + val;
 	Split16(result, high_byte, low_byte);
 }
-
-
-
-
 
 
 
@@ -53,35 +64,7 @@ inline void Sub16(const uint16_t val, uint8_t* const high_byte, uint8_t* const l
 
 
 
-inline uint8_t GetLowByte(const uint16_t value) {
-	return value & 0x00FF;
-}
 
-
-
-
-
-
-inline uint8_t GetHighByte(const uint16_t value) {
-	return (value & 0xFF00) >> 8;
-}
-
-
-
-
-
-
-inline uint8_t GetLowNibble(uint8_t byte) {
-	return byte & 0x0F;
-}
-
-
-
-
-
-inline uint8_t GetHighNibble(uint8_t byte) {
-	return byte & 0xF0;
-}
 
 
 
